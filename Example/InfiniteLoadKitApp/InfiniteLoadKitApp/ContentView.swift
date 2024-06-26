6c28f9424a5b98db92c3312e6f64f27be0474f6c
happyo <xiaxiongzhi@corp.netease.com>
2024-06-26 17:59:24 +0800

    @dev # Init

---
+++ Example/InfiniteLoadKitApp/InfiniteLoadKitApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  InfiniteLoadKitApp
+//
+//  Created by belyenochi on 2024/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ Example/InfiniteLoadKitApp/InfiniteLoadKitApp/InfiniteLoadKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteLoadKitAppApp.swift
+//  InfiniteLoadKitApp
+//
+//  Created by belyenochi on 2024/6/26.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteLoadKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
